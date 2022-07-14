27f1817c8dd5a6bdadcb98141acb8a8572066008
Eric Frederickson <ericfrederickson68@gmail.com>
2022-07-14 13:01:33 -0500

    improved some function names

--- clock.c
+++ clock.c
@@ -2,25 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int tick(int time);
+int increment_time(int time);
+void display_time_with_meridiem(int time);
 void display_time_without_meridiem(int time);
 
 int
-tick(int time)
+increment_time(int time)
 {
 	if (time == 86399)
 		return 0;
 	return time + 1;
-}
-
-void
-display_time_without_meridiem(int time)
-{
-	int hours = time / (60 * 60);
-	int minutes = (time / 60) % 60;
-	int seconds = time % 60;
-
-	printf("%0.2d:%0.2d:%0.2d\n", hours, minutes, seconds);
 }
 
 void
@@ -41,14 +32,24 @@ display_time_with_meridiem(int time)
 	printf("%0.2d:%0.2d:%0.2d %cM\n", hours, minutes, seconds, meridiem);
 }
 
+void
+display_time_without_meridiem(int time)
+{
+	int hours = time / (60 * 60);
+	int minutes = (time / 60) % 60;
+	int seconds = time % 60;
+
+	printf("%0.2d:%0.2d:%0.2d\n", hours, minutes, seconds);
+}
+
 int
 main(int argc, char *argv[])
 {
-	int ctime = 85400; // 24hr time in seconds, max val = 86400
+	int time = 85400; // 24hr time in seconds, max val = 86400
 
 	while (1) {
-		display_time_with_meridiem(ctime);
-		ctime = tick(ctime);
+		display_time_with_meridiem(time);
+		time = increment_time(time);
 		sleep(1); // pause for 1 second
 	}
 
