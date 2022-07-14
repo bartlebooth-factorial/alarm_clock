1c10708746285c548fc6fd32a4fc505e8bffedbf
Eric Frederickson <ericfrederickson68@gmail.com>
2022-07-14 13:50:11 -0500

    added alarm functionality. need non-blocking input from curses to move forward by adding user control

--- clock.c
+++ clock.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,6 +6,7 @@
 int increment_time(int time);
 void display_time_with_meridiem(int time);
 void display_time_without_meridiem(int time);
+void display_alarm(int alarm_time);
 
 int
 increment_time(int time)
@@ -47,9 +49,34 @@ main(int argc, char *argv[])
 {
 	int time = 85400; // 24hr time in seconds, max val = 86400
 
+	bool meridiem = false; // if true, display time with meridiem info (AM/PM)
+
+	bool alarm_on = true; // booolean to determine if alarm will trigger
+	int alarm_time = 85410; // time value at which alarm triggers if alarm_on is true
+	bool alarm_triggered = false; // bool to determine if alarm has been triggered
+
+	/* main clock loop */
 	while (1) {
-		display_time_with_meridiem(time);
+		if (alarm_triggered) {
+			if (meridiem) {
+				display_time_with_meridiem(alarm_time);
+			} else {
+				display_time_without_meridiem(alarm_time);
+			}
+		} else {
+			if (meridiem) {
+				display_time_with_meridiem(time);
+			} else {
+				display_time_without_meridiem(time);
+			}
+		}
+
 		time = increment_time(time);
+
+		if (alarm_on && time == alarm_time) {
+			alarm_triggered = true;
+		}
+
 		sleep(1); // pause for 1 second
 	}
 
