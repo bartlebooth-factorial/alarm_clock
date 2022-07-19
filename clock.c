a7983f555ed619d23ad20b8c46fde3e57d17bb57
Eric Frederickson <ericfrederickson68@gmail.com>
2022-07-18 23:17:39 -0500

    enabled user to turn clock on and off

--- clock.c
+++ clock.c
@@ -110,6 +110,8 @@ display_setting_buffer(int buffer, bool meridiem)
 int
 main(int argc, char *argv[])
 {
+	bool power_on = true; // boolean representing power status of clock. if false, main loop does nothing but wait
+		
 	int time = 85400; // 24hr time in seconds, max val = 86400
 	int time_set_buffer = 0; // a 24hr timeval in seconds, incremented by minutes when user is setting the time
 
@@ -136,113 +138,128 @@ main(int argc, char *argv[])
 	
 	/* main clock loop */
 	while (1) {
-		clear();
+		if (power_on) {
+			clear();
 
-		if (alarm_triggered) {
-			display_alarm(alarm_time, meridiem);
-		} else {
-			if (meridiem) {
-				display_time_with_meridiem(time, alarm_on);
-			} else {
-				display_time_without_meridiem(time, alarm_on);
-			}
-		}
-
-		if (is_setting == 'a') {
-			if (meridiem) {
-				display_setting_buffer(alarm_set_buffer, true);
-			} else {
-				display_setting_buffer(alarm_set_buffer, false);
-			}
-		} else if (is_setting == 't') {
-			if (meridiem) {
-				display_setting_buffer(time_set_buffer, true);
-			} else {
-				display_setting_buffer(time_set_buffer, false);
-			}
-		}
-
-		refresh();
-
-		if (run_count == 9) {
-			time = increment_time(time);
-			run_count = 0;
-		} else {
-			run_count++;
-		}
-
-		if (alarm_on && time == alarm_time) {
-			alarm_triggered = true;
-		}
-
-		key = getch();
-
-		if (key == 113) { // q: quit
-			goto EXIT;
-		} else if (key == 109) { // m: toggle meridiem
-			if (meridiem == false) {
-				meridiem = true;
-			} else {
-				meridiem = false;
-			}
-		} else if (key == 97) { // a: toggle alarm_on
-			if (alarm_on) {
-				alarm_on = false;
-				alarm_triggered = false;
-			} else {
-				alarm_on = true;
-			}
-		} else if (key == 32) { // space: untrigger a triggered alarm
 			if (alarm_triggered) {
-				alarm_triggered = false;
+				display_alarm(alarm_time, meridiem);
+			} else {
+				if (meridiem) {
+					display_time_with_meridiem(time, alarm_on);
+				} else {
+					display_time_without_meridiem(time, alarm_on);
+				}
 			}
-		} else if (key == 115) { // s: set alarm
-			is_setting = 'a';
-		} else if (key == 83) { // S: set time
-			is_setting = 't';
-		} else if (key == 100) { // d: decrease time being set by one minute
+
 			if (is_setting == 'a') {
-				alarm_set_buffer -= 60;
+				if (meridiem) {
+					display_setting_buffer(alarm_set_buffer, true);
+				} else {
+					display_setting_buffer(alarm_set_buffer, false);
+				}
 			} else if (is_setting == 't') {
-				time_set_buffer -= 60;
+				if (meridiem) {
+					display_setting_buffer(time_set_buffer, true);
+				} else {
+					display_setting_buffer(time_set_buffer, false);
+				}
 			}
-		} else if (key == 105) { // i: increase time being set by one minute
-			if (is_setting == 'a') {
-				alarm_set_buffer += 60;
-			} else if (is_setting == 't') {
-				time_set_buffer += 60;
+
+			refresh();
+
+			if (run_count == 9) {
+				time = increment_time(time);
+				run_count = 0;
+			} else {
+				run_count++;
 			}
-		} else if (key == 68) { // D: decrease time being set by one hour
-			if (is_setting == 'a') {
-				alarm_set_buffer -= 3600;
-			} else if (is_setting == 't') {
-				time_set_buffer -= 3600;
+
+			if (alarm_on && time == alarm_time) {
+				alarm_triggered = true;
 			}
-		} else if (key == 73) { // I: increase time being set by one hour
-			if (is_setting == 'a') {
-				alarm_set_buffer += 3600;
-			} else if (is_setting == 't') {
-				time_set_buffer += 3600;
-			}
-		} else if (key == 102) { // f: finish setting time (set time will take effect)
-			if (is_setting == 'a') {
-				alarm_time = alarm_set_buffer;
+
+			key = getch();
+
+			if (key == 113) { // q: quit
+				goto EXIT;
+			} else if (key == 111) { // o: toggle power
+				clear();
+				refresh();
+				power_on = false;
+			} else if (key == 109) { // m: toggle meridiem
+				if (meridiem == false) {
+					meridiem = true;
+				} else {
+					meridiem = false;
+				}
+			} else if (key == 97) { // a: toggle alarm_on
+				if (alarm_on) {
+					alarm_on = false;
+					alarm_triggered = false;
+				} else {
+					alarm_on = true;
+				}
+			} else if (key == 32) { // space: untrigger a triggered alarm
 				if (alarm_triggered) {
 					alarm_triggered = false;
 				}
-				is_setting = 'n';
-			} else if (is_setting == 't') {
-				time = time_set_buffer;
-				is_setting = 'n';
+			} else if (key == 115) { // s: set alarm
+				is_setting = 'a';
+			} else if (key == 83) { // S: set time
+				is_setting = 't';
+			} else if (key == 100) { // d: decrease time being set by one minute
+				if (is_setting == 'a') {
+					alarm_set_buffer -= 60;
+				} else if (is_setting == 't') {
+					time_set_buffer -= 60;
+				}
+			} else if (key == 105) { // i: increase time being set by one minute
+				if (is_setting == 'a') {
+					alarm_set_buffer += 60;
+				} else if (is_setting == 't') {
+					time_set_buffer += 60;
+				}
+			} else if (key == 68) { // D: decrease time being set by one hour
+				if (is_setting == 'a') {
+					alarm_set_buffer -= 3600;
+				} else if (is_setting == 't') {
+					time_set_buffer -= 3600;
+				}
+			} else if (key == 73) { // I: increase time being set by one hour
+				if (is_setting == 'a') {
+					alarm_set_buffer += 3600;
+				} else if (is_setting == 't') {
+					time_set_buffer += 3600;
+				}
+			} else if (key == 102) { // f: finish setting time (set time will take effect)
+				if (is_setting == 'a') {
+					alarm_time = alarm_set_buffer;
+					if (alarm_triggered) {
+						alarm_triggered = false;
+					}
+					is_setting = 'n';
+				} else if (is_setting == 't') {
+					time = time_set_buffer;
+					is_setting = 'n';
+				}
 			}
-		}
 
-		// if user is currently setting a time, make sure their buffer is a valid time
-		// (not negative and not above 86400)
-		if (is_setting == 'a') {
-			alarm_set_buffer = ensure_valid_time(alarm_set_buffer);
-		} else if (is_setting == 't') {
-			time_set_buffer = ensure_valid_time(time_set_buffer);
+			// if user is currently setting a time, make sure their buffer is a valid time
+			// (not negative and not above 86400)
+			if (is_setting == 'a') {
+				alarm_set_buffer = ensure_valid_time(alarm_set_buffer);
+			} else if (is_setting == 't') {
+				time_set_buffer = ensure_valid_time(time_set_buffer);
+			}
+
+		} else {
+			key = getch();
+
+			if (key == 113) { // q: quit
+				goto EXIT;
+			} else if (key == 111) { // o: toggle power
+				power_on = true;
+			}
 		}
 
 		usleep(1e5); // pause for 1/10 second
